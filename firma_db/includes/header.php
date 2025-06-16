1ae0f66863aa12cf458edf572b47c63f670684e9
ErnestIgnys <161514087+ErnestIgnys@users.noreply.github.com>
2025-06-16 22:57:48 +0200

    Add files via upload

--- firma_db/includes/header.php
+++ firma_db/includes/header.php
@@ -1,14 +1,22 @@
-<header>
-    <h1>System Serwisu Samochodowego</h1>
-    <nav>
-        <ul>
-            <li><a href="index.php">Strona Główna</a></li>
-            <li><a href="klienci.php">Klienci</a></li>
-            <li><a href="samochody.php">Samochody</a></li>
-            <li><a href="uslugi.php">Usługi</a></li>
-            <li><a href="pracownicy.php">Pracownicy</a></li>
-            <li><a href="platnosci.php">Płatności</a></li>
-            <li><a href="raporty.php">Raporty</a></li>
-        </ul>
-    </nav>
-</header>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>System Serwisu Samochodowego</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>System Serwisu Samochodowego</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">Strona Główna</a></li>
+                <li><a href="klienci.php">Klienci</a></li>
+                <li><a href="samochody.php">Samochody</a></li>
+                <li><a href="uslugi.php">Usługi</a></li>
+                <li><a href="pracownicy.php">Pracownicy</a></li>
+                <li><a href="platnosci.php">Płatności</a></li>
+                <li><a href="raporty.php">Raporty</a></li>
+            </ul>
+        </nav>
+    </header>
